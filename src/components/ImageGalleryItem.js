ca8d0b3d3a86be4b87a11e3b0921ac2aba625f3c
Volodymyr Udovychenko <vosap2007@i.ua>
2021-01-22 15:04:27 +0200

    API

--- src/components/ImageGalleryItem.js
+++ src/components/ImageGalleryItem.js
@@ -4,24 +4,39 @@ import React, { Component } from 'react';
 export default class ImageGalleryItem extends Component {
   state = {
     photo: null,
+    loading: false,
   };
+  
   componentDidUpdate(prevProps, prevState) {
-    if(prevProps.photoName !== this.props.photoName) {
-      fetch('https://pixabay.com/api/?q={this.props.photoName}&page=1&key=19156356-c40e703531fee6556ca92e5f2&image_type=photo&orientation=horizontal&per_page=12')
+    if (prevProps.photoName !== this.props.photoName) {
+      this.setState({loading: true});
+      fetch(`https://pixabay.com/api/?${this.props.photoName}&page=1&key=19156356-c40e703531fee6556ca92e5f2&image_type=photo&orientation=horizontal&per_page=12`)
       .then(res => res.json())
-      .then(photo => this.setState({photo}));
+      .then(photo => this.setState({photo}))
+      .finally(() => this.setState({loading: false}));
     }
   }
 
    render() { 
      return (
       <div>
-       {this.state.photo && <div>{this.state.photo.tags}</div>}
+        {this.state.loading && <h1>Загружаем...</h1>}
+        {!this.props.photoName && <div>Введите свой запрос.</div>}
+       {this.state.photo && this.state.photo.map(({largeImageURL, webformatURL, id, tags}) =>
+        <li /*className="ImageGalleryItem"*/ key={id}>
+          <img src={webformatURL} alt={tags}  />
+       </li>
+        )
+       }
       </div>
     ); 
   }
 }
+//https://pixabay.com/api/?q=tree&page=1&key=19156356-c40e703531fee6556ca92e5f2&image_type=photo&orientation=horizontal&per_page=12
+/* className="ImageGalleryItem-image"*/ 
 
-/*<li className="ImageGalleryItem">
-  <img src={this.props.photoName} alt="" className="ImageGalleryItem-image" />
-</li> */ 
+/*<div>
+       <li className="ImageGalleryItem">
+       <img src='https://pixabay.com/get/gaf97fddca723beaf8782a81c6fbfe9cb226651d2eee58d2790cd5501c15e6728d2c9e40cc64ebae691ad5856c7b7e3aedbfe10a6a453ea20d11f074a0bfd34c1_640.jpg' alt=""  />
+       </li>
+      </div>*/
